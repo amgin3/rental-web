804cfd5ce4dea274a4849e0b87ff3e7b1fac4d57
Abe Lincoln <A.spagnoletti@eu.aswatson.com>
2018-06-13 17:34:58 +0200

    new commit

---
+++ rental-car-system/src/main/java/com/rentalcars/rentalcarsystem/RentalCarSystemApplication.java
@@ -0,0 +1,12 @@
+package com.rentalcars.rentalcarsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalCarSystemApplication.class, args);
+	}
+}
